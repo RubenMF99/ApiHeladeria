fd5dc5a0a4bcfc2f96d5b9a2c5aa9b096626240f
NataliaArango <92561847+NataliaArango@users.noreply.github.com>
2021-11-23 11:27:34 -0500

    Solo falta controlador de Restaurante

--- Restaurante/Models/Pedido.cs
+++ Restaurante/Models/Pedido.cs
@@ -3,9 +3,9 @@ namespace Restaurante.Models
 {
     public class Pedido
     {
-        public int Idpedido { get; set; }
+        public int IdPedido { get; set; }
         public int Cedula { get; set; }
-        public string Idplato { get; set; }
+        public string IdPlato { get; set; }
         public string Fecha { get; set; }
     }
 }
